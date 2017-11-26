e5be3c4ad6a27bb5c5be9dd0e646c223ffe9b5c8
emppu－cao <759362837@qq.com>
2017-11-26 13:23:07 +0800

    初始化项目，添加流水布局

--- photoBrowserExample/ViewController.swift
+++ photoBrowserExample/ViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
+private let collectonCellID = "collectonCellID"
 class ViewController: UIViewController {
+    
+    fileprivate lazy var collectionView : UICollectionView = {
 
+        let layout = waterfallLayout()
+        layout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10)
+        layout.scrollDirection = .vertical
+        layout.detaSource = self
+    
+        let frame = CGRect(x: 0, y: 64, width: view.frame.width, height: view.frame.height - 64)
+       let collection = UICollectionView(frame: frame, collectionViewLayout: layout)
+        collection.delegate = self
+        collection.dataSource = self
+        collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: collectonCellID)
+        collection.backgroundColor = .white
+      return collection
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.white
+        
+        view.addSubview(collectionView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
     }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: collectonCellID, for: indexPath)
+        let pictureName = "icon" + "\(indexPath.item + 1)"
+        let imageView =  UIImageView(image: UIImage(named: pictureName))
+        imageView.contentMode = UIViewContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        cell.backgroundView = imageView
+        return cell
+    }
+}
 
-
+extension ViewController: waterfallLayoutDataSource{
+    
+    func colsheightWithIdx(_ waterFall: waterfallLayout, itemIdx: Int) -> CGFloat {
+        return  130//CGFloat(arc4random_uniform(120) + 100)
+    }
+    func numberOfCols(_ waterFall: waterfallLayout) -> Int {
+        return 3
+    }
 }
 
---
+++ photoBrowserExample/waterfallLayout.swift
@@ -0,0 +1,71 @@
+//
+//  waterfallLayout.swift
+//  photoBrowserExample
+//
+//  Created by emppu－cao on 2017/11/25.
+//  Copyright © 2017年 魏小庄. All rights reserved.
+//
+
+import UIKit
+
+protocol waterfallLayoutDataSource: class{
+    func numberOfCols(_ waterFall: waterfallLayout) -> Int
+    func colsheightWithIdx(_ waterFall: waterfallLayout, itemIdx: Int) -> CGFloat
+}
+
+
+class waterfallLayout: UICollectionViewFlowLayout {
+    weak var detaSource: waterfallLayoutDataSource?
+    
+    fileprivate lazy var cellAttris: [UICollectionViewLayoutAttributes] = [UICollectionViewLayoutAttributes]()
+    fileprivate lazy var totalHeights: [CGFloat] = Array(repeating: self.sectionInset.top, count: self.cols)
+    fileprivate lazy var cols: Int = {
+        return self.detaSource?.numberOfCols(self) ?? 2
+    }()
+    
+    
+}
+
+extension waterfallLayout{
+    // 重新布局
+    override func prepare() {
+        super.prepare()
+        
+        // 获取所有cell的个数
+        let cellCount = collectionView!.numberOfItems(inSection: 0)
+        
+        let itemW: CGFloat = (collectionView!.bounds.width - sectionInset.left - sectionInset.right - CGFloat(cols - 1) * minimumInteritemSpacing) / CGFloat(cols)
+        // 给每个cell创建一个layoutAttribes
+        for i in cellAttris.count..<cellCount{
+            let attribs = UICollectionViewLayoutAttributes(forCellWith: IndexPath(item: i, section: 0))
+            // 设置attribs 的frame
+            let minH = totalHeights.min()!
+            // 取出当前高度最小的
+            let minIdx = totalHeights.index(of: minH)!
+            
+            let itemX: CGFloat = sectionInset.left + (minimumInteritemSpacing + itemW) * CGFloat(minIdx)
+            let itemY: CGFloat = minH
+            // 高度需外界传入
+            guard let itemH: CGFloat = detaSource?.colsheightWithIdx(self, itemIdx: i) else{
+                fatalError("please return Index Of height")
+            }
+            attribs.frame = CGRect(x: itemX, y: itemY, width: itemW, height: itemH)
+            cellAttris.append(attribs)
+            // 添加当前的高度
+            totalHeights[minIdx] = minH + minimumLineSpacing + itemH
+        }
+        
+    }
+    
+}
+
+extension waterfallLayout{
+    // 返回计算好的所有布局
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        return cellAttris
+    }
+    // 返回contentSize
+    override var collectionViewContentSize: CGSize{
+        return CGSize(width: 0, height: totalHeights.max()! + sectionInset.bottom - minimumLineSpacing)
+    }
+}
